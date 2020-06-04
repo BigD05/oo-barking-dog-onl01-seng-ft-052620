3574fd60fcdce4fec4dfd0e246e2a1c1a2fa19cf
kyle Dornan <kyledornan26@gmail.com>
2020-06-04 14:17:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,13 @@ class Dog
     @this_is_dog = dogs_name
     
   end 
-  
+  def name 
+    @this_is_dog
+  end 
+  def bark 
+    bark 
+    
+    def bark 
+      puts "Woof!"
+    end 
   end # Your code goes here!
